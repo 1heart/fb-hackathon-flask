87e7109d23dae8b07f25d2a409a47d05ac082b06
1heart <yixin1996@gmail.com>
2015-11-20 08:48:36 -0800

    init

---
+++ hello.py
@@ -0,0 +1,32 @@
+import urllib3
+from json import dumps
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import abort
+from flask import make_response
+
+# application = Flask(__name__)
+
+# if __name__ == '__main__':
+#     application.run(debug=True)
+
+import os
+# from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def make_video_data():
+    print(request)
+    params = request.data
+
+    # hash the video
+
+    # upload to Block Chain
+    return params
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
